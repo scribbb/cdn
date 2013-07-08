ccf134586ec21f4dce8c3d0331919c9a70b35bfe
scribbb. <noreply@scribbb.com>
2013-07-08 16:01:12 +0200

    Updated JS

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
 $(document).pjax('a', '#pjax-container');
+  $('#loading').hide();
+  $(document).on('pjax:send', function() {
+    $('#loading').show();
+    $('#pjax-container').fadeTo('fast', 0.5)
+  })
+  $(document).on('pjax:complete', function() {
     $('#loading').hide();
-    $(document).on('pjax:send', function() {
-        $('#loading').show();
-        $('#pjax-container').fadeTo('fast', 0.5)
-    })
-    $(document).on('pjax:complete', function() {
-        $('#loading').hide();
-        $('#pjax-container').fadeTo('fast', 1)
-    })
-    $(document).on('pjax:timeout', function(event) {
-    event.preventDefault()
+    $('#pjax-container').fadeTo('fast', 1)
+  })
+  $(document).on('pjax:timeout', function(event) {
+  event.preventDefault()
 })
+function DisableButton(button){
+  button.disabled = true;
+  button.value = 'Loading';
+  button.form.submit();
+}
